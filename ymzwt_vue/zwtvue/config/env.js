de40d96963ee860ff9220ed80fa987687be60ae4
huyu <292964589@qq.com>
2020-03-10 17:30:23 +0800

    zwtvue框架提交 1、vue代理 2、基础组件

---
+++ ymzwt_vue/zwtvue/config/env.js
@@ -0,0 +1,10 @@
+'use strict'
+// 接口地址配置
+module.exports = {
+  // 生产环境
+  zwt_address_prod: 'http://121.36.154.134:10000/zwt_app',
+  // 开发环境
+  zwt_address_dev: 'http://121.36.154.134:10000/zwt_app',
+  // 测试环境
+  zwt_address_test: 'http://121.36.154.134:10000',
+}
--- ymzwt_vue/zwtvue/src/main.js
+++ ymzwt_vue/zwtvue/src/main.js
@@ -2,9 +2,30 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-
+import router from './router';
+import iView from 'iview';
+import ElementUI from 'element-ui';
+import 'iview/dist/styles/iview.css';
+import 'element-ui/lib/theme-chalk/index.css';
+import store from './store/store';
+import './plugins/jquery-3.3.1.min';
+import './libs/reload1.css';
+import ZkTable from 'vue-table-with-tree-grid'
+import axios from 'axios'
+import 'babel-polyfill'
+import './permission'
+import JsonView from 'jsonview-vue'
+import elementResizeDetectorMaker from "element-resize-detector"
+import 'swiper/css/swiper.css'
+Vue.prototype.$axios=axios;
 Vue.config.productionTip = false
+
+Vue.use(iView);
+Vue.use(ElementUI);
+Vue.use(Router);
+Vue.use(ZkTable);
+Vue.use(JsonView);
+Vue.use(elementResizeDetectorMaker);
 
 /* eslint-disable no-new */
 new Vue({
